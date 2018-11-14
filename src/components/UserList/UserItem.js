0d63132e3d4a9d3c6e71ac7c01262b8c9cbc5ffd
tedtaedongkim <yenafirst91@gmail.com>
2018-11-14 02:35:01 -0500

    Fix user list style

--- src/components/UserList/UserItem.js
+++ src/components/UserList/UserItem.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import UserImage from "./UserImage";
 
+import { mobile, phone } from "../../utils/media";
+import rem from "../../utils/rem";
+
 const StyledLink = styled(Link)`
+  flex: 1 1 calc(100% / 4 - 40px);
+  max-width: calc(100% / 4 - 40px);
+  ${mobile(
+    css`
+      flex: 1 1 calc(100% / 3 - 32px);
+      max-width: calc(100% / 3 - 32px);
+    `
+  )};
+  ${phone(
+    css`
+      flex-basis: calc(100% - 16px);
+      max-width: calc(100% - 16px);
+    `
+  )};
+
   display: flex;
   background: lightGrey;
   border-radius: 8px;
   align-items: center;
   text-decoration: none;
-  flex-basis: calc(100% / 4 - 32px);
   margin: 4px;
   padding: 4px;
 `;
@@ -19,6 +36,11 @@ const UserImageContainer = styled.div`
   width: 128px;
   height: 128px;
   margin-right: 1rem;
+  flex-shrink: 0;
+`;
+
+const StyledDiv = styled.div`
+  overflow: hidden;
 `;
 
 const UserItem = ({ user }) => {
@@ -27,9 +49,9 @@ const UserItem = ({ user }) => {
       <UserImageContainer>
         <UserImage url={user.avatar_url} name={user.login} />
       </UserImageContainer>
-      <div>
+      <StyledDiv>
         <p>{user.login}</p>
-      </div>
+      </StyledDiv>
     </StyledLink>
   );
 };
