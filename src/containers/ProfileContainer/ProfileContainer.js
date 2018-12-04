ee56577378b2a254ecd96366abac8cb2aa86b687
tedtaedongkim <yenafirst91@gmail.com>
2018-12-04 07:02:04 -0500

    Show repositories & followers

--- src/containers/ProfileContainer/ProfileContainer.js
+++ src/containers/ProfileContainer/ProfileContainer.js
@@ -20,7 +20,7 @@ class ProfileContainer extends Component {
   }
 
   render() {
-    const { userProfile } = this.props;
+    const { userProfile, followers, repositories } = this.props;
     if (!userProfile) {
       //TODO:
       return <div>Error</div>;
@@ -30,7 +30,28 @@ class ProfileContainer extends Component {
         <ProfileHeader userProfile={userProfile} />
         <div>
           <div>Repositories</div>
+          <ul>
+            {repositories.map(repository => (
+              <li key={repository.node_id}>
+                <p>{repository.name}</p>
+                <p>{repository.forks_count}</p>
+                <p>{repository.startgazers_count}</p>
+                <p>{repository.watchers_count}</p>
+                <p>{repository.language}</p>
+                <p>{repository.html_url}</p>
+              </li>
+            ))}
+          </ul>
           <div>Followers</div>
+          <ul>
+            {followers.map(follower => (
+              <li key={follower.node_id}>
+                <p>{follower.login}</p>
+                <p>{follower.avatar_url}</p>
+                <p>{follower.html_url}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       </>
     );
@@ -39,7 +60,9 @@ class ProfileContainer extends Component {
 
 const mapStateToProps = state => ({
   userProfile: state.profile.userProfile,
-  isLoading: state.profile.isLoading
+  isLoading: state.profile.isLoading,
+  followers: state.follower.followers,
+  repositories: state.repository.repositories
 });
 
 const mapDispatchToProps = dispatch => ({
