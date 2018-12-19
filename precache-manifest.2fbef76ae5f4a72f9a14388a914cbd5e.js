d513a607f6a0d080c60f054f9198791a9e2e4233
tedtaedongkim <yenafirst91@gmail.com>
2018-12-18 23:00:14 -0500

    Updates

---
+++ precache-manifest.2fbef76ae5f4a72f9a14388a914cbd5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "598d979a69e879bf36e3",
+    "url": "/github-user-finder/static/js/runtime~main.598d979a.js"
+  },
+  {
+    "revision": "405ffca7c56bfcd7ec97",
+    "url": "/github-user-finder/static/js/main.405ffca7.chunk.js"
+  },
+  {
+    "revision": "5412b63e8ef695e71199",
+    "url": "/github-user-finder/static/js/1.5412b63e.chunk.js"
+  },
+  {
+    "revision": "5412b63e8ef695e71199",
+    "url": "/github-user-finder/static/css/1.daa5d1fa.chunk.css"
+  },
+  {
+    "revision": "9b3ffe72ba317b7f61ac7e2613c6374f",
+    "url": "/github-user-finder/index.html"
+  }
+];
